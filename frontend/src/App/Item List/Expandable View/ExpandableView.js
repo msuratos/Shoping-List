d6896dcc4442b1272924f8108904bc3f61466640
Melvin Suratos <melvinsuratos@hotmail.com>
2021-01-17 23:01:46 -0800

    Inserting a <ul> tag when '-' has been pressed

--- frontend/src/App/Item List/Expandable View/ExpandableView.js
+++ frontend/src/App/Item List/Expandable View/ExpandableView.js
@@ -1,9 +1,28 @@
 import React from "react";
+import './expandableview.css';
 
 class ExpandableView extends React.Component {
+    formatText(inputKey) {
+        const key = inputKey.key;
+
+        console.log('key has been pressed', key);
+
+        switch (key) {
+            case '-':
+                inputKey.preventDefault();
+                document.execCommand('insertunorderedlist', false, null);
+                break;
+        
+            default:
+                break;
+        }
+    }
+
     render() {
         return (
-            <div>Hello from expandable view</div>
+            <div id="editContainer" className="container" contentEditable={true} suppressContentEditableWarning={true} onKeyPress={this.formatText}>
+                Hello from expandable view
+            </div>
         );
     }
 }
