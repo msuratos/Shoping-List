4e5d188630bbeaa10b4fa4ea478b9cb8bb20e3ea
Melvin Suratos <72821450+msuratos@users.noreply.github.com>
2021-04-03 21:33:06 -0700

    Feature/api update (#7)
    * Made the service host the static files
    * Removed old packages

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const cookies = require('cookie-parser');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
@@ -11,19 +10,17 @@ const rootRoute = require('./routes/root-route');
 const db = require('./config/db')
 const app = express();
 const port = process.env.PORT;
+const path = __dirname + '/build/';
 
+app.use(express.static(path));
 app.use(helmet());
 app.use(cookies());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
-
-app.get('/', (req, res) => {
-    res.send('Root endpoint of Shopping List API')
-});
 
 app.use('/api/v1', rootRoute);
 
 app.listen(port, () => {
-    console.log(`Backend API for Shopping List is listening at http://localhost:${port}`)
+    console.log(`Backend API for Shopping List is listening at http://localhost:${port}`);
 });
