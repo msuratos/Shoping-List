36f24229260421e15625a9cdfd65137dc6addcbf
Melvin Suratos <melvinsuratos@hotmail.com>
2021-09-09 22:36:18 -0700

    Moved add logic to parent ItemList component - [x] modified UI for better UX

--- src/client/src/Components/ExpandableView.jsx
+++ src/client/src/Components/ExpandableView.jsx
@@ -1,54 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { Button, TextField } from "@material-ui/core";
+import React, {  } from "react";
+import { Accordion, AccordionSummary, AccordionDetails } from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import './ExpandableView.css';
-import { AddCategory, AddItem, GetItemsForUser } from "../Apis/ItemApi";
 
-const ExpandableView = () => {
-    const [items, setItems] = useState([]);
-    const [category, setCategory] = useState('');
-    const [item, setItem] = useState('');
-
-    const addCategory = async () => {
-        const resp = await AddCategory(category);
-        if (!resp.ok) console.log('Error adding category', await resp.json());
-    };
-
-    const addItem = async () => {
-        const resp = await AddItem(item);
-        if (!resp.ok) console.log('Error adding category', await resp.json());
-    };
-    
-    useEffect(() => {
-        const getItemsForUser = async () => {
-            const resp = await GetItemsForUser();
-            
-            if (resp.ok) {
-                const data = await resp.json();
-                setItems(data);
-            }
-        };
-
-        getItemsForUser();
-    }, []);
+const ExpandableView = (props) => {
+    const { items } = props;
 
     return (
         <>
-            <div style={{margin: 5}}>
-                <TextField label="Category" variant="outlined" size="small" 
-                    value={category} onChange={(e) => setCategory(e.target.value)} />
-                <Button variant="contained" onClick={addCategory}>Add +</Button>
-            </div>
-            <div style={{margin: 5}}>
-                <TextField label="Item" variant="outlined" size="small" 
-                    value={item} onChange={(e) => setItem(e.target.value)} />
-                <Button variant="contained" onClick={addItem}>Add +</Button>
-            </div>
-            <ul>
-                {
-                    items.map((item) => <li key={item._id}>{item.category}</li>)
-                }
-            </ul>
+            {
+                items.map((item) => 
+                    <Accordion key={item._id}>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />}
+                            aria-label="Expand"
+                            aria-controls="additional-actions1-content">
+                            {item.category}
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <ul>
+                                {item.items.map((it, i) => <li key={i}>{it}</li>)}
+                            </ul>
+                        </AccordionDetails>
+                    </Accordion>
+                )
+            }
         </>
     );
 }
